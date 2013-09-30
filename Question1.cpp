dd71804ab4f4d5b02b24f1e20b9e29de3b07c30e
1maithripal1 <maithri1@sympatico.ca>
2013-09-30 16:12:34 -0400

    Create Question1.cpp
    Assignment 1 Question 1

---
+++ Question1.cpp
@@ -0,0 +1,26 @@
+#include <iostream.h>
+#include <stdlib.h>
+
+int main()
+{
+int number1;
+int number2;
+int number3;
+int number4;
+int number5;
+int tot;
+    cout << "Enter first number ";
+cin >> number1;
+cout << "Enter second number ";
+cin >> number2;
+cout << "Enter third number ";
+cin >> number3
+cout << "Enter fourth number ";
+cin >> number4;
+cout << "Enter fifth number ";
+cin >> number5;
+tot=number1+number2+number3+number4+number5;
+cout << tot << endl;
+system("PAUSE");
+ return 0;
+}
