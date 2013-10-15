8c52137b56349d4b06b427edef16d626d8377396
1maithripal1 <maithri1@sympatico.ca>
2013-10-15 16:27:12 -0400

    Update Question3.cpp
    Fixed program to accomadate 0

--- Question3.cpp
+++ Question3.cpp
@@ -1,30 +1,37 @@
-#include <iostream.h>
-#include <stdlib.h>
+#include <iostream>
 
-void main()
+#include <stdlib.h>
+using namespace std;
+int main()
 {
-//clrscr();
-int number,count=0;
-cout<<"Enter number to check if it is prime or not? ";
-cin>>number;
+
+int number=0;
+int count=0;
+cout << "Enter number to check if it is prime or not? ";
+cin >> number;
+
+if (number==0) {
+    cout << "Neither" << endl;
+     system("PAUSE");
+}
 
 for(int a=2;a<=number;a++)
 {
-   if(number%a==0)
-{
-count++;
-}
+    if(number%a==0)
+    {
+        count++;
+    }
 }
 
 if(count==1)
 {
-cout<< "Prime Number \n";
+    cout<< "Prime Number \n";
 }
 else
 {
-cout<< "Not Prime Number \n";
+    cout<< "Not Prime Number \n";
 }
-//getch();
+
  system("PAUSE");
    return 0;
 }
